c999a9c06b9116d3e595416391051c477b675c74
migschen <32902412+migschen@users.noreply.github.com>
2026-04-20 19:32:11 +0800

    尝试修复复制问题10
    Fixed clipboard handling by cloning the bitmap to avoid disposal issues.

--- QuickLook.Plugin/QuickLook.Plugin.ImageViewer/NativeMethods/ClipboardEx.cs
+++ QuickLook.Plugin/QuickLook.Plugin.ImageViewer/NativeMethods/ClipboardEx.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 using System.Diagnostics;
 using System.Drawing;
 using System.Drawing.Imaging;
@@ -6,6 +6,8 @@ using System.IO;
 using System.Threading;
 using System.Windows.Media.Imaging;
 using Clipboard = System.Windows.Forms.Clipboard;
+using WFDataObject = System.Windows.Forms.DataObject;
+using WFDataFormats = System.Windows.Forms.DataFormats;
 
 namespace QuickLook.Plugin.ImageViewer.NativeMethods;
 
@@ -30,15 +32,22 @@ internal static class ClipboardEx
                     image = new WriteableBitmap(image);
                 }
 
-                using var bitmap = image.ToBitmap();
-                var data = new System.Windows.Forms.DataObject();
+                using var originalBitmap = image.ToBitmap();
+                
+                // 【关键修复】：必须克隆一份脱离 using 生命周期的 Bitmap 给剪贴板
+                // 否则方法结束时 originalBitmap 被释放，微信/QQ去读就会报错（红X或粘贴失败）
+                var clipboardBitmap = new Bitmap(originalBitmap);
 
+                // 显式使用 WinForms 的 DataObject，避免与 WPF 冲突
+                var data = new WFDataObject();
+
+                // 显式使用 WinForms 的 DataFormats
                 // 标准位图，给微信/QQ/钉钉用
-                data.SetData(DataFormats.Bitmap, bitmap);
+                data.SetData(WFDataFormats.Bitmap, clipboardBitmap);
 
                 // PNG 流，给 Word/浏览器用
                 using var pngStream = new MemoryStream();
-                bitmap.Save(pngStream, ImageFormat.Png);
+                originalBitmap.Save(pngStream, ImageFormat.Png);
                 data.SetData("PNG", pngStream, false);
 
                 Clipboard.SetDataObject(data, true);
@@ -63,6 +72,6 @@ internal static class ClipboardEx
         enc.Frames.Add(BitmapFrame.Create(source));
         enc.Save(outStream);
         using var temp = new Bitmap(outStream);
-        return new Bitmap(temp);
+        return new Bitmap(temp); // 这里内部的 new Bitmap(temp) 已经处理了流关闭的问题，没问题
     }
 }
